d55661c89504a3a7b47e9ef142404d1cf8baadaf
fnascimento <felipe.basina@gmail.com>
2018-10-16 21:53:33 -0300

    Adicionado projeto.

---
+++ spring/spring-jwt-poc/src/main/java/br/com/spring/jwt/poc/springjwtpoc/SpringJwtPocApplication.java
@@ -0,0 +1,12 @@
+package br.com.spring.jwt.poc.springjwtpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtPocApplication.class, args);
+	}
+}
