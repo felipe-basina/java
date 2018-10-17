689c5c6ea399a04ccfd9d52680d31695cdd09c0b
fnascimento <felipe.basina@gmail.com>
2018-10-16 22:06:48 -0300

    Adicionado implementacao do projeto.

--- spring/spring-jwt-poc/src/main/java/br/com/spring/jwt/poc/SpringJwtPocApplication.java
+++ spring/spring-jwt-poc/src/main/java/br/com/spring/jwt/poc/SpringJwtPocApplication.java
@@ -1,4 +1,4 @@
-package br.com.spring.jwt.poc.springjwtpoc;
+package br.com.spring.jwt.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
