0681166c861b9d8a3be4740d54939f5e8f707376
fnascimento <felipe.basina@gmail.com>
2018-12-04 22:33:25 -0200

    Adicionado projeto para testes diversos.

---
+++ spring/spring-diversos-exemplos/src/main/java/br/com/exemplo/springdiversos/springdiversosexemplos/SpringDiversosExemplosApplication.java
@@ -0,0 +1,12 @@
+package br.com.exemplo.springdiversos.springdiversosexemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiversosExemplosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiversosExemplosApplication.class, args);
+	}
+}
