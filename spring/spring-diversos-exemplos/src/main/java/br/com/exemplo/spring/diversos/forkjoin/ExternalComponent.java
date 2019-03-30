ec9849be95a8fd16835a0276b930d33e8d71604b
fnascimento <felipe.basina@gmail.com>
2019-03-30 12:32:28 -0300

    Alterado recuperacao de instancias a serem processadas.

--- spring/spring-diversos-exemplos/src/main/java/br/com/exemplo/spring/diversos/forkjoin/ExternalComponent.java
+++ spring/spring-diversos-exemplos/src/main/java/br/com/exemplo/spring/diversos/forkjoin/ExternalComponent.java
@@ -4,20 +4,24 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalComponent {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
-	private ExternalServiceFactory externalServiceFactory;
+	private List<ExternalService> externalServices;
 	
 	public void callExternalServicesInParallel() {
-		List<ExternalService> externalServices = this.externalServiceFactory.createExternalServices();
 		ForkJoinPool forkJoinPool = new ForkJoinPool(externalServices.size());
 		try {
 			forkJoinPool.submit(() -> externalServices.parallelStream().forEach(externalService -> {
+				logger.info("##### Instance of {} #####", externalService.getClass().getSimpleName());
 				externalService.call();
 			})).get();
 		} catch (InterruptedException e) {
