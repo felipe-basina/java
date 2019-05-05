25f37770ffd13066b6affc36aa294352db804f2d
fnascimento <felipe.basina@gmail.com>
2019-05-05 11:18:02 -0300

    Adicionado arquivo yml com definicao de contratos para api.

--- spring/stock-api-test/src/main/java/com/stock/api/test/StockApiTestApplication.java
+++ spring/stock-api-test/src/main/java/com/stock/api/test/StockApiTestApplication.java
@@ -1,4 +1,4 @@
-package com.stock.api.test.stockapitest;
+package com.stock.api.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
