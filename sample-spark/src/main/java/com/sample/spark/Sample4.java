4c8b5b9c38bd537e20185ad07dc013d02dfd23c4
fnascimento <felipe.basina@gmail.com>
2020-07-24 08:50:09 -0300

    Organizado operacoes em metodos.

--- sample-spark/src/main/java/com/sample/spark/Sample4.java
+++ sample-spark/src/main/java/com/sample/spark/Sample4.java
@@ -11,33 +11,44 @@ public class Sample4 {
     private static final String KEY_SPACE = "gameofthrones";
     private static final String TABLE = "battles";
 
+    private static JavaSparkContext sc;
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf();
         conf.setAppName("Java API demo");
         conf.setMaster("local[*]");
         conf.set("spark.cassandra.connection.host", "127.0.0.1");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc = new JavaSparkContext(conf);
 
-        /*
+        printAttackerKingsName();
+        printBattles();
+
+        sc.close();
+    }
+
+    private static void printAttackerKingsName() {
         JavaRDD<String> cassandraRdd = CassandraJavaUtil.javaFunctions(sc)
                 .cassandraTable(KEY_SPACE, TABLE, CassandraJavaUtil.mapColumnTo(String.class))
                 .select("attacker_king");
 
+        System.out.println("################# Attacker King's Name #################");
+        System.out.println("#########################################################");
         // Print attacker king's name
         for (String attackerKing : cassandraRdd.collect()) {
             System.out.println(" - " + attackerKing);
-        }*/
+        }
+    }
 
+    private static void printBattles() {
         JavaRDD<Battle> battles = CassandraJavaUtil.javaFunctions(sc)
                 .cassandraTable(KEY_SPACE, TABLE, CassandraJavaUtil.mapRowTo(Battle.class))
                 .select("battle_number", "year", "attacker_king", "defender_king");
 
+        System.out.println("################# Battles #################");
+        System.out.println("###########################################");
         for (Battle battle : battles.collect()) {
             System.out.println(battle);
         }
-
-
-        sc.close();
     }
 
 }
