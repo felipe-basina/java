e587d2288fa079434eb8d2768ce803e95ca30d28
fnascimento <felipe.basina@gmail.com>
2020-08-21 21:33:58 -0300

    Adicionado exemplos de operacoes com left e right join.

--- alura-spark2/src/main/java/alura/spark/Join.java
+++ alura-spark2/src/main/java/alura/spark/Join.java
@@ -1,14 +1,14 @@
 package alura.spark;
 
 import alura.spark.models.CSVFileContent;
-import alura.spark.models.IOrder;
 import alura.spark.models.JSONFileContent;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function;
 import scala.Tuple2;
 
 public class Join {
@@ -34,29 +34,47 @@ public class Join {
         JavaPairRDD<Integer, JSONFileContent> siteFileContent = siteFileContent();
         System.out.printf("JSON file = %d\n", siteFileContent.count());
 
+        // Join CSV + JSON
         JavaPairRDD<Integer, Tuple2<CSVFileContent, JSONFileContent>> joinResponse = csvFileContent
                 .join(siteFileContent)
                 .distinct();
-        for (Tuple2<Integer, Tuple2<CSVFileContent, JSONFileContent>> joined: joinResponse.collect()) {
-            System.out.println("ID = " + joined._1());
-            Tuple2<CSVFileContent, JSONFileContent> joinedContent = joined._2();
-            System.out.printf("CSV file content = %s\nJSON file content = %s\n",
-                    joinedContent._1(),
-                    joinedContent._2());
-        }
-        System.out.println("\n\nTotal distincts: " + joinResponse.count());
+//        for (Tuple2<Integer, Tuple2<CSVFileContent, JSONFileContent>> joined: joinResponse.collect()) {
+//            System.out.println("ID = " + joined._1());
+//            Tuple2<CSVFileContent, JSONFileContent> joinedContent = joined._2();
+//            System.out.printf("CSV file content = %s\nJSON file content = %s\n",
+//                    joinedContent._1(),
+//                    joinedContent._2());
+//        }
+        System.out.println("\n\nTotal join (distinct): " + joinResponse.count());
 
-        //joinResponse.collect().forEach(System.out::println);
+        JavaPairRDD<Integer, Tuple2<Optional<CSVFileContent>, JSONFileContent>> rightJoinResponse = csvFileContent
+                .rightOuterJoin(siteFileContent).distinct();
+        System.out.println("\n\nTotal rightJoinResponse: " + rightJoinResponse.count());
 
-//        joinResponse = csvFileContent.intersection(siteFileContent);
-//        System.out.println(joinResponse.count());
+        JavaPairRDD<Integer, Tuple2<CSVFileContent, Optional<JSONFileContent>>> leftJoinResponse = csvFileContent
+                .leftOuterJoin(siteFileContent).distinct();
+        System.out.println("\n\nTotal leftJoinResponse: " + leftJoinResponse.count());
+
+        // Right join :: only json content
+        JavaPairRDD<Integer, Tuple2<Optional<CSVFileContent>, JSONFileContent>> onlyJsonResponse = rightJoinResponse
+                .filter(new Function<Tuple2<Integer, Tuple2<Optional<CSVFileContent>, JSONFileContent>>, Boolean>() {
+                    @Override
+                    public Boolean call(Tuple2<Integer, Tuple2<Optional<CSVFileContent>, JSONFileContent>> tuple) throws Exception {
+                        return !tuple._2()._1().isPresent();
+                    }
+                }).distinct();
+        System.out.println("\n\nTotal onlyJsonResponse: " + onlyJsonResponse.count());
+
+        // Left join :: only csv content
+        JavaPairRDD<Integer, Tuple2<CSVFileContent, Optional<JSONFileContent>>> onlyCsvResponse = leftJoinResponse
+                .filter(new Function<Tuple2<Integer, Tuple2<CSVFileContent, Optional<JSONFileContent>>>, Boolean>() {
+                    @Override
+                    public Boolean call(Tuple2<Integer, Tuple2<CSVFileContent, Optional<JSONFileContent>>> tuple) throws Exception {
+                        return !tuple._2()._2().isPresent();
+                    }
+                }).distinct();
+        System.out.println("\n\nTotal onlyCsvResponse: " + onlyCsvResponse.count());
     }
-
-//    public static JavaRDD<IOrder> csvFileContent() {
-//        return sc.textFile("in/".concat(Files.CONTABILIDADE_CSV_O1.getFileName()))
-//                .filter(content -> !content.contains("id"))
-//                .map(CSVFileContent::new);
-//    }
 
     public static JavaPairRDD<Integer, CSVFileContent> csvFileContent(final String csvFileName) {
         return JavaPairRDD.fromJavaRDD(sc.textFile("in/".concat(csvFileName))
@@ -70,11 +88,6 @@ public class Join {
     private static boolean validCsvContent(final String csvFileContent) {
         return !csvFileContent.contains("id") && !"05:00".equals(csvFileContent);
     }
-
-//    public static JavaRDD<IOrder> siteFileContent() {
-//        return sc.textFile("in/".concat(Files.SITE_JSON.getFileName()))
-//                .map(JSONFileContent::new);
-//    }
 
     public static JavaPairRDD<Integer, JSONFileContent> siteFileContent() {
         return JavaPairRDD.fromJavaRDD(sc.textFile("in/".concat(Files.SITE_JSON.getFileName()))
