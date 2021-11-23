b3d105f0225b2b402954de81accb2c2a8263c34b
afsimao <fsimao@uolinc.com>
2021-11-23 09:17:06 -0300

    Remocao de instancia desnecessaria.

--- spark-lab/src/main/java/spark/lab/general/InitializeContext.java
+++ spark-lab/src/main/java/spark/lab/general/InitializeContext.java
@@ -3,22 +3,20 @@ package spark.lab.general;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 
 public abstract class InitializeContext {
 
     protected static Logger LOGGER = Logger.getLogger(InitializeContext.class);
 
-    protected JavaSparkContext sc;
+    protected static SparkConf conf;
 
     public InitializeContext() {
         this.logging();
 
-        SparkConf conf = new SparkConf()
+        conf = new SparkConf()
                 .setAppName("Java API demo")
                 .setMaster("local[*]")
                 .set("spark.cassandra.connection.host", "127.0.0.1");
-        this.sc = new JavaSparkContext(conf);
     }
 
     private void logging() {
