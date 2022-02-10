f1542e303681d81aec80bbffe077166f14be0214
felipe-basina <felipe.basina@gmail.com>
2022-02-09 23:31:30 -0300

    Adicionado exemplo de geracao de arquivo csv a partir de arquivos parquet, e renomear arquivo gerado.

--- spark-lab/src/main/java/spark/lab/db/cassandra/main/ReadAndWriteJob.java
+++ spark-lab/src/main/java/spark/lab/db/cassandra/main/ReadAndWriteJob.java
@@ -1,5 +1,8 @@
 package spark.lab.db.cassandra.main;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
@@ -9,8 +12,9 @@ import org.apache.spark.sql.SparkSession;
 import spark.lab.db.cassandra.models.User;
 import spark.lab.general.InitializeContext;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Random;
 
 import static spark.lab.general.util.GeneralConstants.SPARK_SQL_CASSANDRA_FORMAT;
 
@@ -27,14 +31,16 @@ public class ReadAndWriteJob extends InitializeContext {
         new ReadAndWriteJob();
 
         try (SparkSession ss = sparkSessionBuilder().getOrCreate()) {
-            printUsersByName(ss);
+            /*printUsersByName(ss);
             addUser(ss, new User(
                     "Doe",
                     "Joe",
                     String.format("joe.doe%d@domain.com", new Random().nextInt(1000))));
             printUsersByName(ss);
-            printTotalGrouped(ss);
+            printTotalGrouped(ss);*/
             generateUserParquetFile(ss);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -86,11 +92,12 @@ public class ReadAndWriteJob extends InitializeContext {
                 .show();
     }
 
-    private static void generateUserParquetFile(SparkSession ss) {
+    private static void generateUserParquetFile(SparkSession ss) throws IOException {
         final String filePath = "/opt/dev/files";
         Dataset<User> userDataset = getUsersByName(ss);
         LOGGER.info("Total users from database = " + userDataset.count());
 
+        // Generates parquet files
         userDataset
                 .write()
                 .option("header", true)
@@ -98,11 +105,34 @@ public class ReadAndWriteJob extends InitializeContext {
                 .parquet(filePath);
         LOGGER.info("File generated at = " + filePath);
 
+        // Read parquet file contents
         Dataset<User> usersFromParquetFile = ss.read()
                 .parquet(filePath)
                 .as(User.encoder());
         LOGGER.info("Total users from file = " + usersFromParquetFile.count());
         usersFromParquetFile.show();
+
+        FileSystem fileSystem = FileSystem.get(ss.sparkContext().hadoopConfiguration());
+        long length = fileSystem.getContentSummary(new Path(filePath)).getLength();
+        long fileLength = length / (1 * 1024 * 1024); // 1MB
+
+        // Write parquet contents in a single csv file
+        usersFromParquetFile
+                .coalesce(1)
+                .write()
+                .option("header", true)
+                .mode(SaveMode.Overwrite)
+                .csv(filePath.concat("csv"));
+
+        // Rename generated csv file
+        FileUtil.copyMerge(
+                fileSystem,
+                new Path(filePath.concat("csv")),
+                fileSystem,
+                new Path(filePath.concat("/new/").concat(String.format("file_%s.csv", LocalDate.now()))),
+                true,
+                ss.sparkContext().hadoopConfiguration(),
+                null);
     }
 
 }
