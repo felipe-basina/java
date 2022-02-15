0e8ce52d2b1b4db810074dea99f74034ec3d394c
felipe-basina <felipe.basina@gmail.com>
2022-02-14 22:59:27 -0300

    Adicionado modelo.

--- spark-lab/src/main/java/spark/lab/files/models/BioStats.java
+++ spark-lab/src/main/java/spark/lab/files/models/BioStats.java
@@ -1,2 +1,24 @@
-package spark.lab.files.models;public class BioStats {
+package spark.lab.files.models;
+
+import lombok.Data;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+
+import java.io.Serializable;
+
+@Data
+public class BioStats implements Serializable {
+
+    private String name;
+
+    private String sex;
+
+    private int height;
+
+    private int weight;
+
+    public static Encoder<BioStats> encoder() {
+        return Encoders.bean(BioStats.class);
+    }
+
 }
