1ddbb2991be4fe4d601b0320323ef1635295b847
afsimao <fsimao@uolinc.com>
2022-12-19 10:49:11 -0300

    Adicionado funcoes utilitarais.

--- spark-lab/src/main/java/spark/lab/general/InitializeContext.java
+++ spark-lab/src/main/java/spark/lab/general/InitializeContext.java
@@ -2,13 +2,20 @@ package spark.lab.general;
 
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.storage.StorageLevel;
 
 public abstract class InitializeContext {
 
     protected static Logger LOGGER = Logger.getLogger(InitializeContext.class);
 
     protected static SparkConf conf;
+
+    protected static final String SEMICOLON_DELIMITER = ";";
 
     public InitializeContext() {
         conf = new SparkConf()
@@ -19,6 +26,48 @@ public abstract class InitializeContext {
 
     protected static SparkSession.Builder sparkSessionBuilder() {
         return SparkSession.builder().config(conf);
+    }
+
+    protected static Dataset<Row> readCSVFiles(SparkSession ss, String filePath, String delimiter) {
+        return ss.read()
+                .option("delimiter", delimiter)
+                .option("header", true)
+                .csv(filePath);
+    }
+
+    protected static Dataset<Row> readParquetFiles(SparkSession ss, String filePath) {
+        return ss.read().parquet(filePath);
+    }
+
+    protected static Dataset<Row> getJoinedDataset(
+            Dataset<Row> leftDataset,
+            Dataset<Row> rightDataset,
+            String leftDatasetColumnName,
+            String rightDatasetColumnName,
+            String join
+    ) {
+        Dataset<Row> joinedDataset = leftDataset.join(rightDataset, rightDataset.col(rightDatasetColumnName)
+                        .equalTo(leftDataset.col(leftDatasetColumnName)), join.toLowerCase())
+                .persist(StorageLevel.MEMORY_ONLY());
+        System.out.println("Total (" + join + "Join): " + joinedDataset.count());
+        return joinedDataset;
+    }
+
+    protected static void writeContentIntoCsvFile(Dataset<Row> dataset, String path, String delimiter) {
+        dataset.repartition(1)
+                .write()
+                .option("delimiter", delimiter)
+                .option("header", true)
+                .csv(path);
+    }
+
+    protected static void showCountGroupedBy(Dataset<Row> dataset, Column column) {
+        System.out.println("Count by " + column.logName());
+        dataset.select(column)
+                .groupBy(column)
+                .agg(functions.count(column))
+                .orderBy(column.asc())
+                .show();
     }
 
 }
