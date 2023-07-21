25f8ec65a7930e24bfb8f5d0e730295d6b106ad1
Felipe Nascimento <felipe.basina@gmail.com>
2023-07-21 14:25:19 -0300

    Adicionado marcacao para gerar construtor padrao.

--- spark-lab/src/main/java/spark/lab/samples/models/SomePojo.java
+++ spark-lab/src/main/java/spark/lab/samples/models/SomePojo.java
@@ -2,9 +2,11 @@ package spark.lab.samples.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SomePojo {
 
     private Integer id;
